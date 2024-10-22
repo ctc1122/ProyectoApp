4ea1af17ae7bb95e9701012a1665ec480a4b3a82
unknown <cristofer.torres.ct@gmail.com>
2024-10-22 09:33:49 +0200

    Añado todos los archivos del proyecto

---
+++ app/src/main/java/com/example/cajamoradamovil/Controlador.java
@@ -0,0 +1,14 @@
+package com.example.cajamoradamovil;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Controlador extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pantalla_principal);
+    }
+}
